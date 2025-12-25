2516cae8c292c43134ec0751b34198f888b143c9
meet2707 <71483049+meet2707@users.noreply.github.com>
2025-12-26 03:05:58 +0530

    Added 24 batch instead of 22 batch
    Removed batch 2022 rendering and added rendering for batch 2024.

--- src/components/Team/Management/Management.jsx
+++ src/components/Team/Management/Management.jsx
@@ -3,42 +3,42 @@ import TeamCard from "../TeamCard/TeamCard";
 import { ManagementTeam } from "../TeamData";
 
 export default function Management() {
+  const batch2023 = ManagementTeam.filter(d => d.batch === "2023");
+  const batch2024 = ManagementTeam.filter(d => d.batch === "2024");
 
-    const batch2022 = ManagementTeam.filter(data => data.batch === "2022");
-    const batch2023 = ManagementTeam.filter(data => data.batch === "2023");
+  return (
+    <div className="memberContainer">
 
-    return (
-        <div className="memberContainer">
-            {/* <h6>Batch of 2022</h6>
-            <div className="members">
-                {batch2022.map(data => (
-                    <TeamCard
-                        key={data.id}
-                        name={data.name}
-                        profile={data.img}
-                        position={data.position}
-                        linkedin={data.linkedin}
-                        github={data.github}
-                        insta={data.insta}
-                    />
-                ))}
-            </div>
-            <br></br>
-            <br></br> */}
-            <h6>Batch of 2023</h6>
-            <div className="members">
-                {batch2023.map(data => (
-                    <TeamCard
-                        key={data.id}
-                        name={data.name}
-                        profile={data.img}
-                        position={data.position}
-                        linkedin={data.linkedin}
-                        github={data.github}
-                        insta={data.insta}
-                    />
-                ))}
-            </div>
-        </div>
-    )
+      <h6>Batch of 2023</h6>
+      <div className="members">
+        {batch2023.map(data => (
+          <TeamCard
+            key={data.id}
+            name={data.name}
+            profile={data.img}
+            position={data.position}
+            linkedin={data.linkedin}
+            github={data.github}
+            insta={data.insta}
+          />
+        ))}
+      </div>
+
+      <h6>Batch of 2024</h6>
+      <div className="members">
+        {batch2024.map(data => (
+          <TeamCard
+            key={data.id}
+            name={data.name}
+            profile={data.img}
+            position={data.position}
+            linkedin={data.linkedin}
+            github={data.github}
+            insta={data.insta}
+          />
+        ))}
+      </div>
+
+    </div>
+  );
 }
