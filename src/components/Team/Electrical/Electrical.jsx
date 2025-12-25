4d3d8b245e9166b21043557a3fa8a4e12ffae193
meet2707 <71483049+meet2707@users.noreply.github.com>
2025-12-26 03:11:13 +0530

    Add support for batch 2024 in Electrical component

--- src/components/Team/Electrical/Electrical.jsx
+++ src/components/Team/Electrical/Electrical.jsx
@@ -4,25 +4,12 @@ import { ElectricalTeam } from "../TeamData";
 
 export default function Electrical() {
 
-    const batch2022 = ElectricalTeam.filter(data => data.batch === "2022");
     const batch2023 = ElectricalTeam.filter(data => data.batch === "2023");
+    const batch2024 = ElectricalTeam.filter(data => data.batch === "2024");
 
     return (
         <div className="memberContainer">
-            {/* <h6>Batch of 2022</h6>
-            <div className="members">
-                {batch2022.map(data => (
-                    <TeamCard
-                        key={data.id}
-                        name={data.name}
-                        profile={data.img}
-                        position={data.position}
-                        linkedin={data.linkedin}
-                        github={data.github}
-                        insta={data.insta}
-                    />
-                ))}
-            </div> */}
+
             <h6>Batch of 2023</h6>
             <div className="members">
                 {batch2023.map(data => (
@@ -37,6 +24,22 @@ export default function Electrical() {
                     />
                 ))}
             </div>
+
+            <h6>Batch of 2024</h6>
+            <div className="members">
+                {batch2024.map(data => (
+                    <TeamCard
+                        key={data.id}
+                        name={data.name}
+                        profile={data.img}
+                        position={data.position}
+                        linkedin={data.linkedin}
+                        github={data.github}
+                        insta={data.insta}
+                    />
+                ))}
+            </div>
+
         </div>
-    )
+    );
 }
