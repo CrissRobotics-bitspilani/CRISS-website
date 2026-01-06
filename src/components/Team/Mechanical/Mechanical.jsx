9f7be9f4ab3252435b639f4054a0672f664cec22
Sarthak Shrivastava <sarthak.doha@gmail.com>
2026-01-07 01:16:41 +0530

    Added 24 batch details

--- src/components/Team/Mechanical/Mechanical.jsx
+++ src/components/Team/Mechanical/Mechanical.jsx
@@ -6,12 +6,13 @@ export default function Mechanical() {
 
     const batch2022 = MechanicalTeam.filter(data => data.batch === "2022");
     const batch2023 = MechanicalTeam.filter(data => data.batch === "2023");
+    const batch2024 = MechanicalTeam.filter(data => data.batch === "2024");
 
     return (
         <div className="memberContainer">
-            {/* <h6>Batch of 2022</h6>
+            <h6>Batch of 2023</h6>
             <div className="members">
-                {batch2022.map(data => (
+                {batch2023.map(data => (
                     <TeamCard
                         key={data.id}
                         name={data.name}
@@ -22,10 +23,10 @@ export default function Mechanical() {
                         insta={data.insta}
                     />
                 ))}
-            </div> */}
-            <h6>Batch of 2023</h6>
+            </div>
+            <h6>Batch of 2024</h6>
             <div className="members">
-                {batch2023.map(data => (
+                {batch2024.map(data => (
                     <TeamCard
                         key={data.id}
                         name={data.name}
